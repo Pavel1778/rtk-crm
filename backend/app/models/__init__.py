ed61d6c635c3ad8d6dff773fe47c25c07e9daafc
RTK Team <team@rtk-crm.ru>
2026-09-17 15:00:03 +0000

    feat: полный проект RTK CRM (backend + frontend) согласно ТЗ хакатона ЛЦТ 2026

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,11 +1,26 @@
-from .university import University
-from .workflow import WorkflowStage
-from .user import User, UserRole
-from .file import AttachedFile
-from .log import ActionLog
-from .comment import Comment
+"""Экспорт всех моделей для Alembic и SQLAlchemy."""
+from app.models.base import Base
+from app.models.user import User, UserRole
+from app.models.university import University
+from app.models.it_product import ITProduct
+from app.models.it_direction import ITDirection
+from app.models.workflow_stage import WorkflowStage
+from app.models.interaction import Interaction
+from app.models.interaction_history import InteractionHistory
+from app.models.action_log import ActionLog
+from app.models.attached_file import AttachedFile
+from app.models.comment import Comment
 
 __all__ = [
-    "University", "WorkflowStage", "User", "UserRole",
-    "AttachedFile", "ActionLog", "Comment"
+    "Base",
+    "User", "UserRole",
+    "University",
+    "ITProduct",
+    "ITDirection",
+    "WorkflowStage",
+    "Interaction",
+    "InteractionHistory",
+    "ActionLog",
+    "AttachedFile",
+    "Comment",
 ]
